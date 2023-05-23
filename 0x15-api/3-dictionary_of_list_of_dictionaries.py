4e005f29a39f90f8acd5b352b8c295c85baa19cb
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-05-23 10:52:56 +0100

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-# csv exported
+"""
+Exports all tasks in the JSON format.
+"""
 import json
-from requests import get
-from sys import argv
+import requests
 
 
-def jsonWrite():
-    """writes to csv"""
-    data = get('https://jsonplaceholder.typicode.com/users').json()
-    ids = [(dic.get('id'), dic.get('username')) for dic in data]
-    dumped = {}
-    for person in ids:
-        data = get(
-            'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
-                person[0])).json()
-        ordered = [{"task": line.get('title'), "completed":
-                    line.get('completed'), "username":
-                    person[1]} for line in data]
-        dumped[person[0]] = ordered
-    with open('todo_all_employees.json', 'w') as f:
-        json.dump(dumped, f)
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    users = requests.get(url).json()
 
+    data = {}
+    for user in users:
+        user_id = str(user.get('id'))
+        data[user_id] = []
 
-if __name__ == "__main__":
-    jsonWrite()
+        user_tasks = requests.get(url + user_id + '/todos').json()
+        for task in user_tasks:
+            task_data = {}
+            task_data['task'] = task.get('title')
+            task_data['completed'] = task.get('completed')
+            task_data['username'] = user.get('username')
+            data[user_id].append(task_data)
+
+    with open('todo_all_employees.json', 'w') as json_file:
+        json.dump(data, json_file)
