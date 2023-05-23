25c07897848b80fb01b405b3f6201a3b996f501a
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-05-23 10:45:08 +0100

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,47 +1,26 @@
 #!/usr/bin/python3
-"""Script to get information from the TODO api endpoint as it
-pertains to a particular employee identified by ID."""
+# csv exported
 import json
-import requests
+from requests import get
+from sys import argv
 
 
-todo_endpoint = "https://jsonplaceholder.typicode.com/todos"
-user_endpoint = "https://jsonplaceholder.typicode.com/users"
-
-
-def get_all_todos():
-    """Get TODO lists for all users identified by `user_id`"""
-    todos = requests.get(todo_endpoint)
-    try:
-        return todos.json()
-    except:
-        exit(1)
-
-
-def get_all_users():
-    """Get all users"""
-    users = requests.get(user_endpoint)
-    try:
-        return users.json()
-    except:
-        exit(1)
-
-
-def export_json_user_todos():
-    """Export employee TODO lists to a json file"""
-    todos = get_all_todos()
-    users = get_all_users()
-    data = {
-        u['id']: [dict(task=todo.get('title'),
-                       completed=todo.get('completed'),
-                       username=u.get('username'))
-                  for todo in filter(
-                          lambda t: t.get('userId') == u.get('id'), todos
-                  )] for u in users
-    }
-    with open("todo_all_employees.json", 'w') as f:
-        json.dump(data, f)
+def jsonWrite():
+    """writes to csv"""
+    data = get('https://jsonplaceholder.typicode.com/users').json()
+    ids = [(dic.get('id'), dic.get('username')) for dic in data]
+    dumped = {}
+    for person in ids:
+        data = get(
+            'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+                person[0])).json()
+        ordered = [{"task": line.get('title'), "completed":
+                    line.get('completed'), "username":
+                    person[1]} for line in data]
+        dumped[person[0]] = ordered
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(dumped, f)
 
 
 if __name__ == "__main__":
-    export_json_user_todos()
+    jsonWrite()
