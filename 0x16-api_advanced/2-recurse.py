fda77fe9e8bddc578f41ced9fe43e79e7c808cf3
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-06-06 13:17:48 +0100

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,46 +1,27 @@
 #!/usr/bin/python3
-import requests
+# get subs
+from requests import get
+from sys import argv
 
-def count_words(subreddit, word_list, after=None, count_dict=None):
-    """Recursively query the Reddit API and count the occurrences of given words in the titles of hot articles."""
 
-    # Base case: no more articles to process
-    if after == "STOP":
-        # Sort and print the count dictionary
-        sorted_count = sorted(count_dict.items(), key=lambda x: (-x[1], x[0]))
-        for word, count in sorted_count:
-            print("{}: {}".format(word, count))
-        return
+def recurse(subreddit, hotlist=[], after=None):
+    """subs"""
+    head = {'User-Agent': 'Dan Kazam'}
+    try:
+        if after:
+            count = get('https://www.reddit.com/r/{}/hot.json?after={}'.format(
+                subreddit, after), headers=head).json().get('data')
+        else:
+            count = get('https://www.reddit.com/r/{}/hot.json'.format(
+                subreddit), headers=head).json().get('data')
+        hotlist += [dic.get('data').get('title')
+                    for dic in count.get('children')]
+        if count.get('after'):
+            return recurse(subreddit, hotlist, after=count.get('after'))
+        return hotlist
+    except:
+        return None
 
-    # First call of the function: initialize count_dict
-    if count_dict is None:
-        count_dict = {word.lower(): 0 for word in word_list}
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-
-    headers = {"User-Agent": "myBot/0.0.1"}
-
-    params = {"limit": 100}
-
-    if after:
-        params["after"] = after
-
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-
-    if response.status_code != 200:
-        return
-
-    data = response.json()
-
-    articles = data.get("data", {}).get("children", [])
-
-    # Update count_dict with the occurrences of the given words in the titles of the articles
-    for article in articles:
-        title = article.get("data", {}).get("title", "").lower()
-        for word in word_list:
-            if " {} ".format(word.lower()) in title:
-                count_dict[word.lower()] += title.count(" {} ".format(word.lower()))
-
-    # Recursive call with the name of the last article processed as the "after" parameter
-    after = articles[-1].get("data", {}).get("name")
-    count_words(subreddit, word_list, after=after, count_dict=count_dict)
+if __name__ == "__main__":
+    recurse(argv[1])
