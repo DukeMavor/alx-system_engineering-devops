c84bd5700f4e86c1efdb57ad2da23294405f7816
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-06-06 13:19:55 +0100

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-# get subs
+"""Function to recursively GET all the hot posts of a give subreddit"""
 from requests import get
-from sys import argv
 
 
-def recurse(subreddit, hotlist=[], after=None):
-    """subs"""
-    head = {'User-Agent': 'Dan Kazam'}
+def recurse(subreddit, hot_posts=[], after=None):
+    """Recursively GET all the hot posts of `subreddit`"""
+    r = get("https://www.reddit.com/r/{}/hot.json".format(subreddit),
+            params={"raw_json": 1, "g": "GLOBAL", "after": after},
+            headers={"User-Agent": "Andrew from Holberton"},
+            allow_redirects=False)
     try:
-        if after:
-            count = get('https://www.reddit.com/r/{}/hot.json?after={}'.format(
-                subreddit, after), headers=head).json().get('data')
-        else:
-            count = get('https://www.reddit.com/r/{}/hot.json'.format(
-                subreddit), headers=head).json().get('data')
-        hotlist += [dic.get('data').get('title')
-                    for dic in count.get('children')]
-        if count.get('after'):
-            return recurse(subreddit, hotlist, after=count.get('after'))
-        return hotlist
+        r.raise_for_status()
     except:
         return None
-
-
-if __name__ == "__main__":
-    recurse(argv[1])
+    else:
+        try:
+            children = r.json().get('data').get('children')
+            hot_posts.extend(map(lambda c: c.get('data').get('title'),
+                                 children))
+            after = r.json().get('data').get('after')
+            if after is None:
+                return hot_posts
+            else:
+                return recurse(subreddit, hot_posts, after=after)
+        except:
+            return None
